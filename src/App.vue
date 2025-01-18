b9322ea3b18ef62b2fd53ebac8a6b7bdd2bd51ac
syf-TuT <2337763816@qq.com>
2025-01-18 22:59:57 +0800

    feat: 按需导入element plus

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <nav>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
